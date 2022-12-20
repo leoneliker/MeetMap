1a6a1bb7325969f70ccaee7f5d320c20cd11be45
Iker Iturralde <ikerite02@gmail.com>
2022-12-20 13:52:52 +0100

    Added activities

---
+++ app/src/main/java/com/example/meetmap/ActivitiesList.kt
@@ -0,0 +1,11 @@
+package com.example.meetmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivitiesList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activities_list)
+    }
+}
