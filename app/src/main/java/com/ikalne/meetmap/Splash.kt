9fae832b6c8ba64ff02e6c0ef2c4768b451dafb1
Iker <ikerite02@gmail.com>
2023-01-11 12:47:57 +0100

    Implemented the initial activity

--- app/src/main/java/com/ikalne/meetmap/Splash.kt
+++ app/src/main/java/com/ikalne/meetmap/Splash.kt
@@ -1,11 +1,22 @@
 package com.ikalne.meetmap
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        openApp();
+    }
+    private fun openApp() {
+        val handler = Handler()
+        handler.postDelayed(Runnable {
+            val intent = Intent(this@Splash, Initial::class.java)
+            startActivity(intent)
+        }, 2000)
     }
 }
