cc69f6f1c871ba34487792ebbb9718629594477c
Nereare4 <nereare4@gmail.com>
2023-01-11 10:28:43 +0100

    Added animations and animated the hands of the splash

--- app/src/main/java/com/ikalne/meetmap/Splash.kt
+++ app/src/main/java/com/ikalne/meetmap/Splash.kt
@@ -2,10 +2,23 @@ package com.ikalne.meetmap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        var leftHand = findViewById<ImageView>(R.id.leftHand)
+        var rightHand = findViewById<ImageView>(R.id.rightHand)
+
+        var animLeftHand = AnimationUtils.loadAnimation(this, R.anim.move_left_to_right)
+        var animRightHand = AnimationUtils.loadAnimation(this, R.anim.move_right_to_left)
+
+        leftHand.startAnimation(animLeftHand)
+        rightHand.startAnimation(animRightHand)
+
     }
 }
