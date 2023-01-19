017b3518c547081f7b98760331f91de24db6a595
Nereare4 <nereare4@gmail.com>
2023-01-19 09:26:36 +0100

    Connection with Firebase database and login and sign up with AuthFirebase (mail + password)

--- app/src/main/java/com/ikalne/meetmap/Login.kt
+++ app/src/main/java/com/ikalne/meetmap/Login.kt
@@ -5,22 +5,66 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Login : AppCompatActivity() {
+
+    lateinit var email: EditText
+    lateinit var password: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
         val login = findViewById<Button>(R.id.btnlogin)
         val cancel = findViewById<Button>(R.id.btncancel)
 
-        login.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this, MapsActivity::class.java)
-            startActivity(intent)
-        })
-        cancel.setOnClickListener(View.OnClickListener {
+
+        login.setOnClickListener{
+            login()
+        }
+        cancel.setOnClickListener{
             val intent = Intent(this, Initial::class.java)
             startActivity(intent)
-        })
+        }
+    }
+    fun login(){
+        if (email.text.isEmpty() && !email.text.contains("@")){
+            showError(email, "Email is not valid")
+        }else if(password.text.isEmpty()){
+            showError(password, "Password can't be empty")
+        }else{ //if (email.text.isNotEmpty() && password.text.isNotEmpty())
+            FirebaseAuth.getInstance()
+                .signInWithEmailAndPassword(email.text.toString(), password.text.toString()).addOnCompleteListener{
+                if (it.isSuccessful){
+                    showMapActivity()
+                }else{
+                    showAlert()
+                }
+            }
+        }
+    }
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error de autenticación al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showError(editText: EditText, error: String){
+        editText.error = error
+    }
+
+    private fun showMapActivity(){
+        val intent = Intent(this, MapsActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/ikalne/meetmap/SignUp.kt
+++ app/src/main/java/com/ikalne/meetmap/SignUp.kt
@@ -5,22 +5,68 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class SignUp : AppCompatActivity() {
+
+    lateinit var email: EditText
+    lateinit var password: EditText
+    lateinit var repassword: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sing_up)
 
-        val login = findViewById<Button>(R.id.btnSingUp)
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
+        repassword = findViewById(R.id.repassword)
+        val signUp = findViewById<Button>(R.id.btnSignUp)
         val cancel = findViewById<Button>(R.id.btncancel)
 
-        login.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this, MapsActivity::class.java)
-            startActivity(intent)
-        })
-        cancel.setOnClickListener(View.OnClickListener {
+
+
+        signUp.setOnClickListener{
+            signUp()
+        }
+        cancel.setOnClickListener{
             val intent = Intent(this, Initial::class.java)
             startActivity(intent)
-        })
+        }
+    }
+    fun signUp(){
+        if (email.text.isNotEmpty() && password.text.isNotEmpty() && repassword.text.isNotEmpty()){
+            if (password.text.toString().equals(repassword.text.toString())){
+                FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(email.text.toString(), password.text.toString()).addOnCompleteListener{
+                    if (it.isSuccessful){
+                        showMapActivity()
+                    }else{
+                        showAlert()
+                    }
+                }
+            }else{
+                Toast.makeText(this, "Please check both passwords", Toast.LENGTH_LONG).show()
+            }
+        }else {
+           Toast.makeText(this, "Please add your credentials", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error de autenticación al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+    private fun showMapActivity(){
+        val intent = Intent(this, MapsActivity::class.java)
+        startActivity(intent)
     }
 }
