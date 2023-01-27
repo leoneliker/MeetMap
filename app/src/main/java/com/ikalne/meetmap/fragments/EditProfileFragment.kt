95ab5fd79a40145e2a9b2b3a9de62ab928f371b4
Almudena Fernandez <fernandezalmuclases@gmail.com>
2023-01-27 10:09:12 +0100

    add glide implementation and circle image profile

--- app/src/main/java/com/ikalne/meetmap/fragments/EditProfileFragment.kt
+++ app/src/main/java/com/ikalne/meetmap/fragments/EditProfileFragment.kt
@@ -2,13 +2,11 @@ package com.ikalne.meetmap.fragments
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.ikalne.meetmap.Initial
 import com.ikalne.meetmap.Login
 import com.ikalne.meetmap.MeetMapApplication
@@ -45,6 +43,11 @@ lateinit var binding: FragmentEditProfileBinding
 //        binding.apply {
 //            btnlogout.s
 //        }
+        Glide.with(this) //.load("https://images.unsplash.com/photo-1512849934327-1cf5bf8a5ccc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80")
+            .load(R.drawable.cara)
+            .circleCrop()
+            .into(binding.ivuser)
+
     }
 
 
