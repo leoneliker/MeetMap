d46f3c01c49ea3aff554d97784e128ed2275d30f
Almudena Fernandez <fernandezalmuclases@gmail.com>
2023-02-17 10:01:22 +0100

    Contact us with functionality, image work in progress for fragments under construction smaller, hide item new plan

--- app/src/main/java/com/ikalne/meetmap/fragments/ConctactUsFragment.kt
+++ app/src/main/java/com/ikalne/meetmap/fragments/ConctactUsFragment.kt
@@ -1,60 +1,66 @@
 package com.ikalne.meetmap.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.SystemClock
+import android.text.TextUtils.replace
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
+import com.ikalne.meetmap.Login
+import com.ikalne.meetmap.MainAppActivity
 import com.ikalne.meetmap.R
+import com.ikalne.meetmap.databinding.FragmentConctactUsBinding
+import com.ikalne.meetmap.databinding.FragmentEditProfileBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ConctactUsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ConctactUsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    lateinit var binding: FragmentConctactUsBinding
+    private val handler = Handler()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_conctact_us, container, false)
+        binding = FragmentConctactUsBinding.inflate(inflater, container, false)
+        initUI()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ConctactUsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ConctactUsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    fun initUI() {
+        clearFields()
+        binding.btnsend.setOnClickListener {
+            if (!binding.email.text.contains("@")) {
+                showError(binding.etemail, "Email is not valid")
             }
+            else {
+                Toast.makeText(requireActivity(), "Form submitted successfully", Toast.LENGTH_LONG)
+                .show()
+                changeActivity()
+            }
+        }
+        binding.btncancel.setOnClickListener {
+            changeActivity()
+        }
+    }
+
+    fun changeActivity()
+    {
+        val intent = Intent(requireActivity(), MainAppActivity::class.java)
+        startActivity(intent)
+    }
+    fun clearFields()
+    {
+        binding.nombre.text.clear()
+        binding.email.text.clear()
+        binding.suggestion.text.clear()
+    }
+
+    private fun showError(textInputLayout: TextInputLayout, error: String) {
+        textInputLayout.error = error
     }
 }
