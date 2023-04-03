5b6265fd00d1fea15907ac520c9675297bae6a6b
Almudena Fernandez <fernandezalmuclases@gmail.com>
2023-04-03 10:52:08 +0200

    chat list in fragment chat, change style chat conversation and button send

--- app/src/main/java/com/ikalne/meetmap/fragments/ChatFragment.kt
+++ app/src/main/java/com/ikalne/meetmap/fragments/ChatFragment.kt
@@ -8,32 +8,22 @@ import android.view.View
 import android.view.ViewGroup
 import com.ikalne.meetmap.ChatActivity
 import com.ikalne.meetmap.ListOfChatsActivity
+import com.ikalne.meetmap.PreferencesManager
 import com.ikalne.meetmap.R
 import com.ikalne.meetmap.databinding.FragmentChatBinding
-import com.ikalne.meetmap.databinding.FragmentConctactUsBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ikalne.meetmap.models.Chat
+import com.ikalne.meetmap.adapters.ChatAdapter
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ChatFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ChatFragment : Fragment() {
     lateinit var binding: FragmentChatBinding
-    private var param1: String? = null
-    private var param2: String? = null
+    private var user = ""
+    private var db = Firebase.firestore
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,9 +36,73 @@ class ChatFragment : Fragment() {
     }
 
     fun initUI() {
-        binding.openChat.setOnClickListener{
-            val intent = Intent(activity, ListOfChatsActivity::class.java)
-            startActivity(intent)
+
+        user= PreferencesManager.getDefaultSharedPreferences(binding.root.context).getEmail()
+
+        if (user.isNotEmpty()){
+            initViews()
         }
     }
+
+
+    private fun initViews(){
+        binding.newChatButton.setOnClickListener { newChat() }
+
+        binding.listChatsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.listChatsRecyclerView.adapter =
+            ChatAdapter { chat ->
+                chatSelected(chat)
+            }
+
+        val userRef = db.collection("users").document(user)
+
+        userRef.collection("chats")
+            .get()
+            .addOnSuccessListener { chats ->
+                val listChats = chats.toObjects(Chat::class.java)
+
+                (binding.listChatsRecyclerView.adapter as ChatAdapter).setData(listChats)
+            }
+
+        userRef.collection("chats")
+            .addSnapshotListener { chats, error ->
+                if(error == null){
+                    chats?.let {
+                        val listChats = it.toObjects(Chat::class.java)
+
+                        (binding.listChatsRecyclerView.adapter as ChatAdapter).setData(listChats)
+                    }
+                }
+            }
+    }
+
+    private fun chatSelected(chat: Chat){
+        val intent = Intent(requireContext(), ChatActivity::class.java)
+        intent.putExtra("chatId", chat.id)
+        intent.putExtra("user", user)
+        startActivity(intent)
+    }
+
+    private fun newChat(){
+        val chatId = UUID.randomUUID().toString()
+        val otherUser = binding.newChatText.text.toString()
+        val users = listOf(user, otherUser)
+
+        val chat = Chat(
+            id = chatId,
+            name = "Chat con $otherUser",
+            users = users
+        )
+
+        db.collection("chats").document(chatId).set(chat)
+        db.collection("users").document(user).collection("chats").document(chatId).set(chat)
+        db.collection("users").document(otherUser).collection("chats").document(chatId).set(chat)
+
+        val intent = Intent(requireContext(), ChatActivity::class.java)
+        intent.putExtra("chatId", chatId)
+        intent.putExtra("user", user)
+        startActivity(intent)
+    }
+
+
 }
