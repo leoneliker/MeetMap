5ecfe6e9b6460737c8a498a815ea1ee9db79c309
SoySandyYT <100490621+SoySandyYT@users.noreply.github.com>
2023-04-27 10:06:22 +0200

    Added recyclerview and classes

---
+++ app/src/main/java/com/ikalne/meetmap/fragments/SuscribersAdapter.kt
@@ -0,0 +1,41 @@
+package com.ikalne.meetmap.fragments
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ikalne.meetmap.R
+import com.ikalne.meetmap.Suscriber
+
+class SuscribersAdapter(private val suscribersList: MutableList<Suscriber> = mutableListOf()) :
+    RecyclerView.Adapter<SuscribersAdapter.SuscriberViewHolder>() {
+
+    class SuscriberViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
+
+        fun bind(suscriber: Suscriber) {
+            nameTextView.text = suscriber.name
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuscriberViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item_suscriber, parent, false)
+        return SuscriberViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SuscriberViewHolder, position: Int) {
+        holder.bind(suscribersList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return suscribersList.size
+    }
+
+    fun updateList(newList: List<Suscriber>) {
+        suscribersList.clear()
+        suscribersList.addAll(newList)
+        notifyDataSetChanged()
+    }
+}
---
+++ app/src/main/java/com/ikalne/meetmap/fragments/SuscribersFragment.kt
@@ -0,0 +1,54 @@
+package com.ikalne.meetmap.fragments
+
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import com.ikalne.meetmap.R
+import com.ikalne.meetmap.Suscriber
+
+class SuscribersFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: SuscribersAdapter
+    private lateinit var plAct: plAct
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_suscribers, container, false)
+        recyclerView = view.findViewById(R.id.recyclerView)
+        adapter = SuscribersAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        // Obtener la lista de suscriptores de Firebase
+        val suscribersRef = Firebase.firestore
+            .collection("Activities")
+            .document("plAct.id")
+            .collection("Suscribers")
+        suscribersRef.addSnapshotListener { snapshot, e ->
+            if (e != null) {
+                Log.w(TAG, "Listen failed.", e)
+                return@addSnapshotListener
+            }
+            val suscribersList = mutableListOf<Suscriber>()
+            for (doc in snapshot!!.documents) {
+                val suscriber = doc.toObject<Suscriber>()
+                suscriber?.let { suscribersList.add(it) }
+            }
+            adapter.updateList(suscribersList)
+        }
+        return view
+    }
+}
