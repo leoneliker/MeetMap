0715978f758ef0caa1f764070937f3b7171c877a
Almudena Fernandez <fernandezalmuclases@gmail.com>
2023-05-08 21:49:48 +0200

    wip: test splash view

--- app/src/main/java/com/ikalne/meetmap/Splash.kt
+++ app/src/main/java/com/ikalne/meetmap/Splash.kt
@@ -1,15 +1,16 @@
 package com.ikalne.meetmap
 
 import android.content.Intent
-import android.content.pm.ActivityInfo
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
+import android.view.View
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
-import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,12 +23,29 @@ class Splash : AppCompatActivity() {
 
         val leftHand = findViewById<ImageView>(R.id.leftHand)
         val rightHand = findViewById<ImageView>(R.id.rightHand)
+        val mUniverse = findViewById<ImageView>(R.id.backView)
 
         val animLeftHand = AnimationUtils.loadAnimation(this, R.anim.move_left_to_right)
         val animRightHand = AnimationUtils.loadAnimation(this, R.anim.move_right_to_left)
+        val animBack = AnimationUtils.loadAnimation(this, R.anim.fadeinout)
+        leftHand.visibility = View.INVISIBLE
+        rightHand.visibility = View.INVISIBLE
+        animBack.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
 
-        leftHand.startAnimation(animLeftHand)
-        rightHand.startAnimation(animRightHand)
+            override fun onAnimationRepeat(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+                leftHand.visibility = View.VISIBLE
+                rightHand.visibility = View.VISIBLE
+                leftHand.startAnimation(animLeftHand)
+                rightHand.startAnimation(animRightHand)
+            }
+        })
+
+        mUniverse.startAnimation(animBack)
+
+
 
     }
 
@@ -42,7 +60,7 @@ class Splash : AppCompatActivity() {
                 }
                 startActivity(intent)
             }
-        }, 3000)
+        }, 10000)
     }
 
     override fun onPause() {
