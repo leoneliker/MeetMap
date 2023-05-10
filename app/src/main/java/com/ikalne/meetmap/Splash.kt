fe12fd9b7f05c9189940ea64d333b194ebc2f32f
Almudena Fernandez <fernandezalmuclases@gmail.com>
2023-05-10 14:05:26 +0200

    update splash amim with slogan

--- app/src/main/java/com/ikalne/meetmap/Splash.kt
+++ app/src/main/java/com/ikalne/meetmap/Splash.kt
@@ -2,14 +2,22 @@ package com.ikalne.meetmap
 
 import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.util.Log
+import android.view.View
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
 
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,10 +30,64 @@ class Splash : AppCompatActivity() {
 
         val leftHand = findViewById<ImageView>(R.id.leftHand)
         val rightHand = findViewById<ImageView>(R.id.rightHand)
+        val title = findViewById<ImageView>(R.id.meetmap)
+        val slogan = findViewById<TextView>(R.id.slogan)
+        val text = slogan.text.toString()
 
+        val spannableString = SpannableString(text)
+        val words = text.split(" ")
+
+        var colorIndex = 0
+        var startIndex = 0
+
+        for (word in words) {
+            val endIndex = startIndex + word.length
+
+            val color = if (colorIndex % 2 == 0) {
+                R.color.secondary
+            } else {
+               R.color.primary_dark
+            }
+            val colors = ContextCompat.getColor(this, color)
+
+            // Cambia el color de cada palabra
+            spannableString.setSpan(
+                ForegroundColorSpan(colors),
+                startIndex,
+                endIndex,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+
+            startIndex = endIndex + 1 // Añade 1 para ignorar el espacio
+            colorIndex++
+        }
+        slogan.text = spannableString
         val animLeftHand = AnimationUtils.loadAnimation(this, R.anim.move_left_to_right)
         val animRightHand = AnimationUtils.loadAnimation(this, R.anim.move_right_to_left)
+        val animtext = AnimationUtils.loadAnimation(this, R.anim.fadein)
 
+        title.visibility =  View.INVISIBLE
+        slogan.visibility = View.INVISIBLE
+
+        val handsAnimationListener = object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+
+            override fun onAnimationEnd(animation: Animation?) {
+                // Se ejecuta cuando la animación de las manos termina
+                title.visibility = View.VISIBLE
+                slogan.visibility = View.VISIBLE
+                title.startAnimation(animtext)
+                slogan.startAnimation(animtext)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+        }
+
+// Establece el AnimationListener en ambas animaciones de las manos
+        animLeftHand.setAnimationListener(handsAnimationListener)
+        animRightHand.setAnimationListener(handsAnimationListener)
+
+// Inicia las animaciones de las manos
         leftHand.startAnimation(animLeftHand)
         rightHand.startAnimation(animRightHand)
 
@@ -42,7 +104,7 @@ class Splash : AppCompatActivity() {
                 }
                 startActivity(intent)
             }
-        }, 3000)
+        }, 4500)
     }
 
     override fun onPause() {
